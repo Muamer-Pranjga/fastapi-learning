9170592c16785405fbc3396e72f403c468908a3d
Muamer Pranjga <muamer.pranjga@protonmail.com>
2026-02-25 14:33:47 -0500

    add contact endpoint

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ def home():
 @app.get("/about")
 def about():
     return {"name": "Muamer", "role": "developer"}
+
+@app.get("/contact")
+def contact():
+    return {"name": "Muamer", "email": "muamer.92@gmail.com", "contact": "6476478899"}
